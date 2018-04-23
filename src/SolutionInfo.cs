d7babaadb73f82e55bf5d8ccf5c93c1a3ff1b8e0
Balazs Gallay <balazsgallay@mbp-balazs.local>
2018-04-23 11:35:36 +0200

    Build script changes

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("FuryTechs.FloatingActionButton")]
-[assembly: AssemblyVersion("0.0.2")]
-[assembly: AssemblyFileVersion("0.0.2")]
-[assembly: AssemblyInformationalVersion("0.0.2")]
+[assembly: AssemblyVersion("0.0.3")]
+[assembly: AssemblyFileVersion("0.0.3")]
+[assembly: AssemblyInformationalVersion("0.0.3")]
 [assembly: AssemblyCopyright("Copyright (c) 2018 FuryTechs")]
 
