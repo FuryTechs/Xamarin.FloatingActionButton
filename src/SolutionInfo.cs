82817ada1f827aef3affa83ebca65dc55b3bd69b
Balazs Gallay <balazsgallay@mbp-balazs.local>
2018-04-23 10:51:13 +0200

    Release 0.0.2 created

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -6,12 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyProduct("FuryTechs.FloatingActionButton")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
-[assembly: AssemblyInformationalVersion("0.0.1")]
-<<<<<<< HEAD
-[assembly: AssemblyCopyright("Copyright (c) 2015 - 2018 Chase Florell")]
-=======
-[assembly: AssemblyCopyright("Copyright (c) 2015 - 2018 FuryTechs")]
->>>>>>> 653bc1e3e9afc77c0aa11275a2a5f4cf2d470d79
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
+[assembly: AssemblyInformationalVersion("0.0.2")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 FuryTechs")]
 
